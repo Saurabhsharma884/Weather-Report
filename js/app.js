f3ba6030b4dcda0a8708cdfa83602216adfd8596
Saurabh Sharma <saurabh182000@gmail.com>
2021-11-19 14:21:03 +0530

    Local Storage

--- js/app.js
+++ js/app.js
@@ -1,10 +1,25 @@
-//  init weather obj
-const weather = new Weather("Delhi");
-
 //init ui
 const ui = new UI();
 
+const storage = new Storage();
+const weatherLocation = storage.getLocationData();
+
+//  init weather obj
+const weather = new Weather(weatherLocation);
+
 document.addEventListener("DOMContentLoaded", getWeather);
+
+// change location addEventListener
+document.getElementById("w-change-btn").addEventListener("click", (e) => {
+  const city = document.getElementById("city").value;
+
+  weather.changeLocation(city);
+  storage.setLocationData(city);
+
+  getWeather();
+
+  $("#locModal").modal("hide");
+});
 
 function getWeather() {
   weather
