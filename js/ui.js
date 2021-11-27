a2bac36702a5be4bd966788045ce9bdedaf0cae9
Saurabh Sharma <saurabh182000@gmail.com>
2021-11-27 09:22:41 +0530

    UI Changes

--- js/ui.js
+++ js/ui.js
@@ -3,7 +3,6 @@ class UI {
     this.location = document.getElementById("w-location");
     this.desc = document.getElementById("w-desc");
     this.string = document.getElementById("w-string");
-    this.details = document.getElementById("w-details");
     this.icon = document.getElementById("w-icon");
     this.humidity = document.getElementById("w-humidity");
     this.feelsLike = document.getElementById("w-feels-like");
